34cb270ddb29d3cd3be920f0ec141296092072cd
TAYAGCH <tayagch@oocl.com>
2020-10-26 21:55:59 +0800

    [reactjs-counter][christian] feature: add MultiCounter

--- src/components/MultiCounter.js
+++ src/components/MultiCounter.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import CounterGroup from './CounterGroup.js'
 import CounterSizeGenerator from './CounterSizeGenerator.js'
+import CounterGroupSum from './CounterGroupSum.js'
 
 class MultiCounter extends Component {
 
     constructor(props) {
         super(props);
         
-        this.state = { size: 0 };
+        this.state = { size: 0, sum: 0};
     }
     
     onGenerate = (size) => {
         this.setState({ size });
     }
 
+    onSum = (value) => {
+        this.setState(() => ({sum: value}))
+    }
+
     render() {
         return (
             <div>
                 <CounterSizeGenerator onGenerate={this.onGenerate}/>
-                <CounterGroup size={this.state.size} />
+                <CounterGroupSum toShow={this.state.sum}/>
+                <CounterGroup size={this.state.size} onSum={this.onSum}/>
             </div>
         );
     }
